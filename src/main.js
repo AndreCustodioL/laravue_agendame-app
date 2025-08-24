630d3f6548ba273a028e4e7077ffe8446f21c62d
AndreCustodioL <andre.custodio@outlook.com>
2025-08-24 12:17:41 -0300

    Finalização Módulo 3 Migração do Tema Modernize

--- src/main.js
+++ src/main.js
@@ -1,23 +1,13 @@
-/**
- * main.js
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import '@/scss/style.scss';
+import vuetify from './plugins/vuetify';
 
-// Plugins
-import { registerPlugins } from '@/plugins'
-
-// Components
-import App from './App.vue'
-
-// Composables
-import { createApp } from 'vue'
-
-// Styles
-import 'unfonts.css'
 
 const app = createApp(App)
 
-registerPlugins(app)
-
-app.mount('#app')
+app
+    .use(router)
+    .use(vuetify)
+    .mount('#app')
